adbec0d23104a83751f7bc1ea9e21a62f3996d82
Anthony Bloomer <ant0@protonmail.ch>
2016-12-01 19:59:53 +0000

    Create config.py

---
+++ python/config.py
@@ -0,0 +1,11 @@
+# Twitter application settings
+consumer_key = ''
+consumer_secret = ''
+access_key = ''
+access_secret = ''
+
+# Database settings
+host = '127.0.0.1'
+user = 'root'
+password = ''
+db = 'trump'
