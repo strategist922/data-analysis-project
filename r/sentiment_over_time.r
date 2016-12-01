c00da623fab1c57b630418feb1e246c99cee7924
Anthony Bloomer <ant0@protonmail.ch>
2016-12-01 19:55:18 +0000

    Create sentiment_over_time.r

---
+++ r/sentiment_over_time.r
@@ -0,0 +1,18 @@
+library(RMySQL)
+library(ggplot2)
+library(reshape2)
+
+mydb <- dbConnect(MySQL(), user='root', password='', dbname='trump', host='127.0.0.1')
+
+rs <- dbSendQuery(mydb, "select distinct day(created_at) as d, round(avg(pos), 3) as pos_avg, round(avg(neg), 3) as neg_avg 
+                         from tweets as t 
+                         join twitter_sentiment_analysis as tsa 
+                         on tsa.tweet_id = t.id group by d")
+data <- fetch(rs, n=-1)
+
+x <- data$d
+y <- data$pos_avg  
+z <- data$neg_avg
+
+df <- data.frame(x, y, z)
+ggplot(df, aes(x, y = value, color = variable)) + geom_point(aes(y = y, col = "y")) + geom_point(aes(y = z, col = "z"))
