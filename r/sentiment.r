aaae872a05b7ec1ef81f8cdef60b0c1c76d687ae
Anthony Bloomer <ant0@protonmail.ch>
2017-02-19 14:08:44 +0000

    Update sentiment.r

--- r/sentiment.r
+++ r/sentiment.r
@@ -1,4 +1,7 @@
-install.packages('plotly')
+list.of.packages <- c("RMySQL", "plotly")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if (length(new.packages)) install.packages(new.packages)
+  
 library(RMySQL)
 library(plotly)
 
