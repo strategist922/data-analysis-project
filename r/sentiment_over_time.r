64ef82f2b77f3127a8f0e931c34d86dc7d2d06f0
Anthony Bloomer <ant0@protonmail.ch>
2017-02-19 14:09:02 +0000

    Update sentiment_over_time.r

--- r/sentiment_over_time.r
+++ r/sentiment_over_time.r
@@ -1,3 +1,7 @@
+list.of.packages <- c("RMySQL", "ggplot2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if (length(new.packages)) install.packages(new.packages)
+
 library(RMySQL)
 library(ggplot2)
 library(reshape2)
