9558e9a266c5b672731e69591285a4da68d75e9d
Anthony Bloomer <ant0@protonmail.ch>
2017-02-19 14:08:21 +0000

    Update mean.r

--- r/mean.r
+++ r/mean.r
@@ -1,3 +1,6 @@
+list.of.packages <- c("RMySQL", "plotly")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if (length(new.packages)) install.packages(new.packages)
 
 library(RMySQL)
 library(plotly)
